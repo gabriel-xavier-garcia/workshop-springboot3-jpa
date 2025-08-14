bcf45f7cfdc09613e653af38a7f93eecc3932294
Gabriel Garcia <gabrielproaj@gmail.com>
2025-08-14 00:21:19 -0300

    OrderItem, many-to-many association with extra attributes PART 2 with corretion

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -17,14 +17,12 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     private Integer orderStatus;
 
     @ManyToOne
-    @JsonIgnore
     @JoinColumn(name = "client_id")
     private User client;
 
